749d796344e013c1f17d3fabf4f0ab3e9d85c64b
Nikolay Zaytsev <madcmk2@gmail.com>
2024-12-02 15:08:55 +0300

    Keycloak 16 and 24

--- compose/postgres/docker-entrypoint-initdb.d/init-db.sh
+++ compose/postgres/docker-entrypoint-initdb.d/init-db.sh
@@ -2,8 +2,9 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE keycloak;
-	
+	CREATE DATABASE keycloak16;
+	CREATE DATABASE keycloak24;
 	CREATE USER keycloak WITH ENCRYPTED PASSWORD 'keycloak';
-	GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+	GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak16;
+	GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak24;
 EOSQL
